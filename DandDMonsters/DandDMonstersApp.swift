a86c18b7756a3ed3f72bfe6f1bbb4ea0dc596a03
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-10 21:20:30 +0000

    Initial Commit

---
+++ DandDMonsters/DandDMonstersApp.swift
@@ -0,0 +1,18 @@
+//
+//  DandDMonstersApp.swift
+//  DandDMonsters
+//
+//  Created by app-kaihatsusha on 10/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct DandDMonstersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
